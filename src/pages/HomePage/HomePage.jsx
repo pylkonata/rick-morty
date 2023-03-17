01176338d1821e305eb0fbe28b27df76ffcada8e
pylkonata <pylko.nata@gmail.com>
2023-03-17 12:20:45 +0200

    feat: add google login logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import logo from '../../assets/img/rick_morty.png';
 import './HomePage.scss';
 
+import jwt_decode from 'jwt-decode';
+
 import CharactersList from '../../components/CharactersList/CharactersList';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
@@ -11,6 +13,8 @@ const HomePage = ({ onCharSelected }) => {
     : '';
 
   const [search, setSearch] = useState(startSearch);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState('');
 
   const onSearch = (value) => {
     setSearch(value);
@@ -20,9 +24,52 @@ const HomePage = ({ onCharSelected }) => {
     localStorage.setItem('search', search);
   }, [search]);
 
+  useEffect(() => {
+    window.google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: handleCredentialResponse,
+    });
+    window.google.accounts.id.renderButton(
+      document.getElementById('logInBtn'),
+      {
+        theme: 'filled_blue',
+        size: 'small',
+        text: 'signin_with',
+        locale: 'en_US',
+      }
+    );
+  }, [isLoggedIn]);
+
+  const handleCredentialResponse = (response) => {
+    const responsePayload = jwt_decode(response.credential);
+    setUser(responsePayload);
+    setIsLoggedIn(true);
+  };
+  const onLogOut = () => {
+    window.google.accounts.id.revoke(user.sub);
+    setIsLoggedIn(false);
+  };
+
   return (
     <div className='home-page'>
-      <img src={logo} alt='rick and morty' className='home-page__logo' />
+      <header className='header'>
+        <nav className='header__nav'>
+          {isLoggedIn ? (
+            <p className='header__text'>
+              Welcome,
+              <span> {user.name}</span>
+            </p>
+          ) : (
+            <div id='logInBtn'></div>
+          )}
+          {isLoggedIn && (
+            <button className='header__logout-btn' onClick={onLogOut}>
+              Log out
+            </button>
+          )}
+        </nav>
+        <img src={logo} alt='rick and morty' className='header__logo' />
+      </header>
       <SearchBar onSearch={onSearch} startSearch={startSearch} />
       <CharactersList search={search} />
     </div>
